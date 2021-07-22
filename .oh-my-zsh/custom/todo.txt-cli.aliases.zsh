0fbc23f06ef8b1be2b7015bee3d8812a1d8e581c
NickVeld <NickVeld@users.noreply.github.com>
2021-07-22 17:06:32 +0300

    todo.txt-cli: installation and configs

---
+++ .oh-my-zsh/custom/todo.txt-cli.aliases.zsh
@@ -0,0 +1,9 @@
+alias todo.sh='todo.sh -c -t'
+
+# Autocompetion for the alias (other from todo.sh) if it is enabled 
+#complete -F _todo todo.sh_alias
+
+alias    TODO='todo.sh add'
+alias    DONE='todo.sh do'
+alias   TASKS='todo.sh list'
+alias TASKPRI='todo.sh pri'
---
+++ installations/todo.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/todotxt/todo.txt-cli.git ${HOME}/todo.txt-cli
+
+cd ${HOME}/todo.txt-cli
+
+make
+make install
